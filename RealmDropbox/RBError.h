a40e408525b1586a716511e37040b909c0726ee2
Ken Franklin <ken.franklin@toptal.com>
2017-03-13 14:50:49 -0400

    Comment added

--- RealmDropbox/RBError.h
+++ RealmDropbox/RBError.h
@@ -4,9 +4,13 @@
 //  Created by Ken Franklin on 3/12/17.
 //
 //
-
 #import <Foundation/Foundation.h>
 #import <ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h>
+
+
+/* 
+ Converts a Dropbox V2 style error into out own class.
+ */
 
 typedef NS_ENUM(NSInteger, MTErrorCode) {
     MTErrorUnknown = 9000,
@@ -49,6 +53,11 @@ typedef NS_ENUM(NSInteger, MTErrorCode) {
 
 + (RBError *) errorWithDBErrorObject:(NSObject *) errorObject;
 
+/**
+ Add a description of what the app was doing when the error occured.  This will be display or reported to MixPanel / Crashlytics
+
+ @param descriptionOfWhatAppWasAttempting Text of activity occuring at time of error
+ */
 - (void) displayErrorWhileAttempting:(NSString *) descriptionOfWhatAppWasAttempting;
 
 - (void) reportErrorWhileAttempting:(NSString *) descriptionOfWhatAppWasAttempting;
